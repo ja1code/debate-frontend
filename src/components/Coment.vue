66f13b6bcab506f7dd2be79064ddd056fcec1be2
João Pedro Freitas Sá <jp.freitas202@gmail.com>
2019-07-10 23:51:20 -0300

    1° commit com estrutura básica

---
+++ src/components/Coment.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'coment'
+}
+</script>
+
+<style lang="css">
+    
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'main'
+}
+</script>
+
+<style>
+  @font-face {
+    font-family: 'wrangell';
+    src: url(../assets/fonts/wrangell.otf) format('opentype');
+  }
+
+  .header {
+    font-family: 'wrangell';
+  }
+
+  .bg {
+    background-color: #FFFAE2;
+  }
+</style>
