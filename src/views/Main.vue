586a1ad71be82c65e731745c1b00b6c16575520a
Gustavo Henrique Freitas <gustavo.hfn.2001@gmail.com>
2019-07-13 01:10:55 -0300

    coment developing (almost done)

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,19 +1,22 @@
 <template>
   <div class='bg'>
-    <img src='../assets/debate_logo.png'>
+    <!--<img src='../assets/debate_logo.png'>
     <h1><b>Tema do dia:</b></h1>
     <h2 style='margin-bottom: 2%'>Seu pai tem gonorréia?</h2>
-    <feed/>
+    <feed/>-->
+    <coment/>
   </div>
 </template>
 
 <script>
+import coment from '../components/Coment.vue'
 import feed from '../components/Feed.vue'
 
 export default {
   name: 'main',
   components:{
     feed,
+    coment,
   }
 }
 </script>
